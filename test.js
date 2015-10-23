ee7d6bfc503639992391bc89582cbccf4412ba0b
Milian Wolff <milian.wolff@kdab.com>
2015-10-23 16:24:19 +0200

    Use phantomjs url

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 console.log('Loading a web page');
 var page = require('webpage').create();
-var url = 'https://123365.org.xycv.aert.xfv.d/';
+var url = 'http://phantomjs.org';
 page.open(url, function (status) {
   console.log("status callback: " + status);
   console.log("eval ret val:" + page.evaluate(function() {
