8b0af99aefc006a8812e3a5f3e60f12d1756d15f
Milian Wolff <milian.wolff@kdab.com>
2015-10-26 15:48:42 +0100

    Extend promise example, showing that continuation works

--- examples/promises.js
+++ examples/promises.js
@@ -1,27 +1,28 @@
-var promise = new Promise(function(resolve, reject) {
+function createPromise(pass)
+{
+  return new Promise(function(resolve, reject) {
   // do a thing, possibly async, then…
-
   setTimeout(function() {
-    var foo = 0;
-    if (foo) {
+    if (pass) {
       resolve("Stuff worked!");
     } else {
       reject(Error("It broke"));
     }
   }, 500);
 });
+}
+
+var promise = createPromise(true);
 
 promise.then(
-  function() { console.log("yes!"); },
-  function() { console.log("meh!"); }
+  function() { console.log("yes!" + JSON.stringify(arguments)); },
+  function() { console.log("meh!" + JSON.stringify(arguments)); }
 );
 
 promise.then(
-  function() { console.log("ye2s!"); },
-  function() { console.log("me2h!"); }
+  function() { console.log("ye2s!" + JSON.stringify(arguments)); return createPromise(false); },
+  function() { console.log("me2h!" + JSON.stringify(arguments)); }
 ).then(
-  function() { console.log("ye3s!"); },
-  function() { console.log("me3h!"); }
-);
-
-promise.then( phantom.exit, phantom.exit );
+  function() { console.log("ye3s!" + JSON.stringify(arguments));},
+  function() { console.log("me3h!" + JSON.stringify(arguments)); }
+).then( phantom.exit, phantom.exit );
